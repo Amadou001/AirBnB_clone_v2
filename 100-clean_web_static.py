a06e83fc566f6d836d66e50e65cc8a5efdbce481
Amadou001 <bamadou634@gmail.com>
2024-07-14 04:28:49 +0800

    AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,9 @@ from fabric.api import env, local, run
 # Environment configuration
 env.hosts = ['54.144.139.236', '54.242.186.62']
 env.user = 'ubuntu'  # Update with your username
-env.key_filename = '/path/to/your/ssh/private_key'  # Update with your SSH key path
+# Update with your SSH key path
+env.key_filename = '/path/to/your/ssh/private_key'
+
 
 def do_clean(number=0):
     """
@@ -21,14 +23,16 @@ def do_clean(number=0):
 
         # Clean up local versions directory
         versions_dir = 'versions'
-        local_clean_command = 'ls -t {}/ | tail -n +{} | xargs -I {{}} rm -f {}/{{}}'.format(
+        local_clean_command = 'ls -t {}/ | tail -n +{}\
+              | xargs -I {{}} rm -f {}/{{}}'.format(
             versions_dir, number + 1, versions_dir
         )
         local(local_clean_command)
 
         # Clean up remote releases directory
         releases_dir = '/data/web_static/releases'
-        remote_clean_command = 'ls -t {} | tail -n +{} | xargs -I {{}} rm -rf {}/{{}}'.format(
+        remote_clean_command = 'ls -t {} | tail -n +{}\
+              | xargs -I {{}} rm -rf {}/{{}}'.format(
             releases_dir, number + 1, releases_dir
         )
         run(remote_clean_command)
